47857b2bd79a16743aab24ee4783f55f70b5ef14
Miguel <miguel.rrc@hotmail.es>
2015-02-28 05:48:24 +0100

    Initial Commit

---
+++ Weather-forecast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather-forecast
+//
+//  Created by Miguel Rodriguez Rubio on 28/2/15.
+//  Copyright (c) 2015 miguelrrc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
